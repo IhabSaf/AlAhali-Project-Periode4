8fb8ae968a7b80434fc5ffc3bf861f05b19f2997
Aaldert Kroes <a.a.kroes@st.hanze.nl>
2023-06-13 14:14:14 +0200

    idk

--- src/Controller/HistoricalDataController.php
+++ src/Controller/HistoricalDataController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Measurement;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class HistoricalDataController extends AbstractController
 {
     #[Route('/historical/data', name: 'app_historical_data')]
-    public function index(): Response
+    public function index(EntitymanagerInterface $entityManager): Response
     {
+        $data = $entityManager->getRepository(Measurement::class)->findBy();
         return $this->render('historical_data/index.html.twig', [
             'controller_name' => 'HistoricalDataController',
         ]);

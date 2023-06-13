2c33b892373470bcec3fa90c660a379d39ebe7f5
Koen Sikkema <koensikkema@hotmail.com>
2023-06-13 18:47:54 +0200

    fixed cldc per day
    in progress: no duplicate stations

--- src/Controller/CloudinessController.php
+++ src/Controller/CloudinessController.php
@@ -8,48 +8,64 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Date;
 
 class CloudinessController extends AbstractController
 {
+//    #[Route('/cloudiness', name: 'app_cloudiness')]
+//    public function index(EntityManagerInterface $entityManager): Response
+//    {
+//
+//        $data = $entityManager->getRepository(Measurement::class)->findBy([], array('timestamp' => 'DESC', 'cldc' => 'DESC'), 10);
+//        $data->
+//        $data_arr = [];
+//        foreach ($data as $_data ){
+//            $cldc = $_data->getCldc();
+//            $station = $_data->getStationName();
+//            $time_stamp = $_data->getTimestamp()->format('Y-m-d H:i:s');
+//            $data_arr[] = array(
+//                0 => $station,
+//                1 => $cldc,
+//                2 => $time_stamp
+//            );
+//        }
+//        return $this->render('cloudiness/index.html.twig', [
+//            'controller_name' => 'CloudinessController',
+//            'arr' => $data_arr,
+//
+//        ]);
+
     #[Route('/cloudiness', name: 'app_cloudiness')]
     public function index(EntityManagerInterface $entityManager): Response
     {
-      $data = $entityManager->getRepository(Measurement::class)->findBy([], array('timestamp' => 'DESC', 'cldc' => 'DESC'), 10);
         $data_arr = [];
-        foreach ($data as $_data ){
-            $cldc = $_data->getCldc();
-            $station = $_data->getStationName();
-            $time_stamp = $_data->getTimestamp()->format('Y-m-d H:i:s');
-            $data_arr[] = array(
-                0 => $station,
-                1 => $cldc,
-                2 => $time_stamp
-            );
-        }
+        $full_data_arr = $this->filterData($entityManager, $data_arr);
+        $today = date("Y-m-d");
+//        return new Response("<pre>". var_dump($full_data_arr));
         return $this->render('cloudiness/index.html.twig', [
             'controller_name' => 'CloudinessController',
-            'arr' => $data_arr,
+            'arr' => $full_data_arr,
 
         ]);
 
 
-
-        //######################################## trying random things to get distinct working
-
-//        $query = $entityManager->createQuery('select DISTINCT station_name, cldc, timestamp
-//                                                    from ahli_bank.measurement
-//                                                    order by cldc DESC');
-
-    //######################################## trying random things to get distinct working
-
-//        $query = $entityManager->createQueryBuilder();
-//        $query->select('station_name', 'cldc', 'timestamp')->distinct()
-//                ->from('measurement', "m");
-//
-//        $nieuwequery = $query->getDQL();
-//
-////        $buildquery =  $entityManager->createQuery($nieuwequery);
-////        $komop = $buildquery->getResult();
-//        return new Response("<pre>".var_dump($nieuwequery));
+    }
+    public function filterData(EntityManagerInterface $entityManager, array $data_arr): array
+    {
+        $today = date("Y-m-d");
+        $data = $entityManager->getRepository(Measurement::class)->findBy([], array('cldc' => 'DESC', 'timestamp' => 'DESC'), 1000);
+        foreach ($data as $filter_data) {
+            if ($filter_data->getTimestamp()->format('Y-m-d') == $today ) { // && !data_arr($filter_data->getStationName()) 
+                $cldc = $filter_data->getCldc();
+                $station = $filter_data->getStationName();
+                $time_stamp = $filter_data->getTimestamp()->format('Y-m-d H:i:s');
+                $data_arr[] = array(
+                    0 => $station,
+                    1 => $cldc,
+                    2 => $time_stamp
+                );
+            }
+        }
+        return $data_arr;
     }
 }

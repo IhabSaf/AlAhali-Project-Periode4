6cfe29d7be04848c32dfe9ba2042725fd3b12428
K Sikkema <koensikkema@hotmail.com>
2023-06-14 16:35:23 +0200

    start with airpressure

--- src/Controller/AirpressureController.php
+++ src/Controller/AirpressureController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,35 @@ use Symfony\Component\Routing\Annotation\Route;
 class AirpressureController extends AbstractController
 {
     #[Route('/airpressure', name: 'app_airpressure')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entitymanager): Response
     {
+        // query to get the high airpressure
+        $qb = $entitymanager->createQueryBuilder();
+        $qb->select('m.stationName', 'm.stp', 'm.timestamp')
+            ->from('App\Entity\Measurement', 'm')
+            ->where('m.stp > 1029')
+            ->orderBy('m.timestamp', 'DESC')
+            ->setMaxResults(100);
+        $query = $qb->getQuery();
+        $result_high_stp = $query->getResult();
+
+
+
+            // query to get the high airpressure
+        $qb = $entitymanager->createQueryBuilder();
+        $qb->select('m.stationName', 'm.stp', 'm.timestamp')
+            ->from('App\Entity\Measurement', 'm')
+            ->where('m.stp < 990')
+            ->orderBy('m.timestamp', 'DESC')
+            ->setMaxResults(100);
+               $query = $qb->getQuery();
+        $result_low_stp = $query->getResult();
+
+
         return $this->render('airpressure/index.html.twig', [
             'controller_name' => 'AirpressureController',
+            'lowStp' => $result_low_stp,
+            'highStp' => $result_high_stp
         ]);
     }
 }

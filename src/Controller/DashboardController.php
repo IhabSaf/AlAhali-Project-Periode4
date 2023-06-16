0aa1b1daf864aa5c9d6a75a6072602bb735b4f18
Ihab <safadi_i@yahoo.com>
2023-06-16 21:30:52 +0200

    Map

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,33 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+
+
+        $qb = $entityManager->createQueryBuilder();
+        $qb->select('m.stationName', 'm.longitude', 'm.latitude')
+            ->from('App\Entity\Measurement', 'm')
+            ->orderBy('m.stationName','DESC')
+            ->setMaxResults(500);
+        $query = $qb->getQuery();
+        $result = $query->getResult();
+        dump($result[1]);
+
+
+
+        $points = [];
+        foreach ($result as $index => $cord) {
+            $longitude = $cord["longitude"];
+            $latitude = $cord["latitude"];
+            $points[$index] = [$latitude, $longitude];
+        }
+
+        dump($points);
+
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
+            'points' => $points
         ]);
     }
 }

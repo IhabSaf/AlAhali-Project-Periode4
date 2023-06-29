ececc513d9491e758d5bdbae7c358948faa45761
Nicky Houter <101206236+nickyboy1179@users.noreply.github.com>
2023-06-29 18:06:39 +0200

    DownloadXmlController.php works way better now (IMO 😘). Does require running composer.json to get the necessary extension. XML format no longer has data in the tags. Uses id's for this instead (similar to how HTML works).

--- src/Controller/DownloadXmlController.php
+++ src/Controller/DownloadXmlController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 // All imports
 use App\Entity\Measurement;
-use Spatie\ArrayToXml\ArrayToXml;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,42 +11,56 @@ use Symfony\Component\Routing\Annotation\Route;
 class DownloadXmlController extends AbstractController
 {
     #[Route('/download/xml', name: 'download_xml_four_weeks')]
-    public function downloadXml(EntityManagerInterface $em) {
+    public function downloadXml(EntityManagerInterface $em): \Symfony\Component\HttpFoundation\BinaryFileResponse|\Symfony\Component\HttpFoundation\RedirectResponse
+    {
 
         // Als de user niet ingelogd dan wordt hij verwijst weer naar de inlog pagina
         if (!$this->getUser()) {return $this->redirectToRoute('app_login');}
 
-//        $qb = $em->createQueryBuilder();
-//        $qb
-//            ->select('m')
-//            ->from('App\Entity\Measurement', 'm')
-//            ->join('App\Entity\Stations', 's', Join::WITH, 'm.stationName = s.stationName')
-//        ;
-//        $query = $qb->getQuery();
-//        $Results = $query->getArrayResult();
-        $Results = $em->getRepository(Measurement::class)->findAll();
-        dump($Results);
+        $results = $em->getRepository(Measurement::class)->findAll();
 
-        $xml = ArrayToXml::convert(['Historical_data_Ahli_Bank' => $this->createXMLArray($Results)]);
-        $xmlFile = fopen('../templates/four_weeks_xml/four_weeks_data.xml', 'w') or die("File can not be accessed");
-        fwrite($xmlFile, $xml);
-        fclose($xmlFile);
+        $writer = xmlwriter_open_uri('../templates/four_weeks_xml/four_weeks_data.xml');
+        xmlwriter_set_indent($writer, true);
+        xmlwriter_set_indent_string($writer, '  ');
+
+        xmlwriter_start_document($writer, '1.0', 'UTF-8');
+        xmlwriter_start_element($writer, 'root');
+
+        foreach($results as $measurement) {
+            $this->createMeasurementElement($writer, $measurement);
+        }
+
+        xmlwriter_end_element($writer);
+        xmlwriter_end_document($writer);
+
+        xmlwriter_flush($writer);
 
         return $this->file('../templates/four_weeks_xml/four_weeks_data.xml');
     }
 
-    private function createXMLArray(array $results): array {
-        $returnArray = [];
-        for($i = 0; $i < count($results); $i++){
-            $returnArray["measurement_".$results[$i]["measurementId"]] = [
-                'station_name' => $results[$i]["stationName"],
-                'timestamp' => substr($results[$i]["timestamp"]->format('Y-m-d H:i:s'), 0, 19),
-                'longitude' => $results[$i]["longitude"],
-                'latitude' => $results[$i]["latitude"],
-                'stp' => $results[$i]["stp"],
-                'cldc' => $results[$i]["cldc"]
-            ];
-        }
-        return $returnArray;
+    private function createMeasurementElement($writer, $measurement):void {
+        xmlwriter_start_element($writer, 'measurement');
+
+        xmlwriter_start_attribute($writer, 'measurement_id');
+        xmlwriter_text($writer,  $measurement->getMeasurementId());
+        xmlwriter_end_attribute($writer);
+
+        xmlwriter_start_element($writer, 'station_name');
+        xmlwriter_text($writer,  $measurement->getStationName()->getStationName());
+        xmlwriter_end_element($writer);
+
+        xmlwriter_start_element($writer, 'timestamp');
+        xmlwriter_text($writer,  $measurement->getTimestamp()->format('Y-m-d H:i:s'));
+        xmlwriter_end_element($writer);
+
+        xmlwriter_start_element($writer, 'stp');
+        xmlwriter_text($writer, $measurement->getStp());
+        xmlwriter_end_element($writer);
+
+        xmlwriter_start_element($writer, 'cldc');
+        xmlwriter_text($writer, $measurement->getCldc());
+        xmlwriter_end_element($writer);
+
+        xmlwriter_end_element($writer);
     }
 }

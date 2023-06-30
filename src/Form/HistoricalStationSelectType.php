0199375875caf6e814973de01dde362d438bd1df
Nicky Houter <101206236+nickyboy1179@users.noreply.github.com>
2023-07-01 01:54:48 +0200

    ChoiceType form now implemented on HistoricalStationSelectType.php
    Stared work on mobile friendly scaling on historical data page.
    By using fake copies of the sidenav and topnav a responsive layout can be created.

--- src/Form/HistoricalStationSelectType.php
+++ src/Form/HistoricalStationSelectType.php
@@ -3,21 +3,42 @@
 namespace App\Form;
 
 // All imports
+use App\Entity\Stations;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class HistoricalStationSelectType extends AbstractType {
+
+    public function __construct(private EntityManagerInterface $entityManager) {}
     /**
      * Builds a form that asks for a station_name
      * @param FormBuilderInterface $builder
      * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options){
+        $choices = $this->getChoices();
         $builder
-            ->add('stationName', TextType::class, ['required' => true])
+            ->add('stationName', ChoiceType::class, [
+                'choices' => [
+                    'Choose a station' => 'No station selected',
+                    'choices' => $choices
+                    ],
+            ])
             ->add('Submit', SubmitType::class)
         ;
+    }
+
+    private function getChoices():array
+    {
+        $stations = $this->entityManager->getRepository(Stations::class)->findAll();
+        $stationNames = [];
+        foreach ($stations as $station) {
+            $stationName = $station->getStationName();
+            $stationNames[$stationName] = $stationName;
+        }
+        return $stationNames;
     }
 }
